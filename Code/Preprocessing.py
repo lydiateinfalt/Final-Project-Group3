bcac95d148d37bdd2d7c22d1214a14b2f1094c2c
ariannadunham <adunham@gwu.edu>
2021-03-26 15:15:56 -0400

    Updated the reading of the files to read from data_cleanup (misinterpreted and thought we wanted to read the csvs from scratch.

--- Code/Preprocessing.py
+++ Code/Preprocessing.py
@@ -1,23 +1,11 @@
 # Group 3 Preprocessing
 
 # Arianna Code Start:
-import pandas as pd
 import data_cleanup
-# import other libraries below:
+import pandas as pd
 
-# This is my directory on my personal computer so I've commented out and added other lines below for
-# group members to paste their own directory.
-# in_dc = pd.read_csv('/Users/ariannadunham/Desktop/Data Mining DATS 6103/Project/Crashes_in_DC.csv')
-# details = pd.read_csv('/Users/ariannadunham/Desktop/Data Mining DATS 6103/Project/Crash_Details_Table.csv')
-
-# Uncomment and paste directory below
-# in_dc = pd.read_csv()
-# details = pd.read_csv()
 
 crash = data_cleanup.data
-print(crash.shape)
-# Dropping duplicate entries in CRIMEID(the unique identifier for this set) column of the main dataset (in_dc)
-#in_dc = data.drop_duplicates(subset=['CRIMEID'])
 
 # Checking to see the number of crashes involving someone 100+ to determine if that's a good
 # Age to cap at
@@ -33,10 +21,3 @@ plate_filter = (crash.LICENSEPLATESTATE == 'OT') | (crash.LICENSEPLATESTATE == '
 crash = crash.where(plate_filter)
 
 # Arianna end (all code written by me)
-# Filter YEAR  < 2008 and YEAR > 2021
-print("Before dropping data for YEAR < 2008 and YEAR > 2021")
-print(crash.shape)
-crash= crash[(crash.YEAR <= 2021) | (crash.YEAR >= 2016)]
-print("Data shape after filtering YEAR")
-print(crash.shape)
-
