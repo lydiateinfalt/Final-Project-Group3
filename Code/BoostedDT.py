227e1204a83f64086c0cdb247133e4c98331d1df
RyeAnne <RyeAnne@users.noreply.github.com>
2021-04-16 12:07:47 -0400

    Models working but need optimized

--- Code/BoostedDT.py
+++ Code/BoostedDT.py
@@ -1,4 +1,9 @@
 # RR
+import webbrowser
+
+from pydotplus import graph_from_dot_data
+from sklearn.tree import export_graphviz
+
 import Preprocessing
 from sklearn.model_selection import GridSearchCV
 from sklearn.model_selection import StratifiedShuffleSplit
@@ -24,16 +29,16 @@ class gboost:  # class
 
     def accuracy(self):  # this makes the model and finds the accuracy, confusion matrix, and prints the decision tree
 
-        clf = GradientBoostingClassifier(n_estimators=50,
+        clf = GradientBoostingClassifier(n_estimators=1000,
                                         learning_rate=0.05,
-                                        max_depth=7,
+                                        max_depth=10,
                                         min_samples_split=2,
                                         min_samples_leaf=2,
                                         random_state = 50)
         X_train, X_test, y_train, y_test = train_test_split(self.xtrain, self.ytrain, test_size=0.3, random_state=100)
         clf.fit(X_train, y_train)
         y_pred = clf.predict(X_test)
-        self.roc = roc_auc_score(y_test, clf.predict_proba(X_test)[:, 1])
+        self.roc = roc_auc_score(y_test, clf.predict_proba(X_test)[:, 1]) # get AUC value
         self.acc = accuracy_score(y_test, y_pred) * 100  # get the accuracy of the model
         print('The AUC of the model is:', self.roc)
         print('The classification accuracy is:', self.acc)
@@ -52,14 +57,14 @@ class gboost:  # class
         plt.tight_layout()
         plt.show()
 
-        dot_data = export_graphviz(clf, filled=True, rounded=True, class_names=class_names,
-                                   feature_names=self.x.iloc[:, :].columns, out_file=None)
+        # dot_data = export_graphviz(clf, filled=True, rounded=True, class_names=class_names,
+        #                            feature_names=self.xtrain.iloc[:, :].columns, out_file=None)
+        #
+        # graph = graph_from_dot_data(dot_data)
+        # graph.write_pdf("GBoost Decision Tree")
+        # webbrowser.open_new(r'GBoost Decision Tree')
 
-        graph = graph_from_dot_data(dot_data)
-        graph.write_pdf("decision_tree_gini.pdf")
-        webbrowser.open_new(r'decision_tree_gini.pdf')
+        return self.roc  # return the accuracy
 
-        return self.acc# return the accuracy
-
-m = gboost(model)  # set your roman numeral
+m = gboost(model)
 m.accuracy()
